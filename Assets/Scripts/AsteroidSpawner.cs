b28ec3f6e8ecbbdbb0924062fd2a22afcce1895a
Laurie <elaurllc@gmail.com>
2021-09-20 13:41:43 -0400

    Vert and Horiz speeds, input handler in event system

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -7,7 +7,9 @@ public class AsteroidSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] asteroidPrefabs;
     [SerializeField] private float secBetweenAsteroids = 1.5f;
-    [SerializeField] private Vector2 forceRange;
+    [SerializeField] private Vector2 topForce = new Vector2 (2f,4f);
+    [SerializeField] private Vector2 sideForce = new Vector2(4f, 7f);
+
 
     private Camera mainCamera;
     private float timer;
@@ -33,6 +35,7 @@ public class AsteroidSpawner : MonoBehaviour
 
         Vector2 spawnPoint = Vector2.zero;
         Vector2 direction = Vector2.zero;
+        Vector2 force = Vector2.zero;
 
         switch (side)
         {
@@ -41,24 +44,28 @@ public class AsteroidSpawner : MonoBehaviour
                 spawnPoint.x = 0;
                 spawnPoint.y = UnityEngine.Random.value;
                 direction = new Vector2(1f, UnityEngine.Random.Range(-1, 1));
+                force = sideForce;
                 break;
             case 1:
                 //right
                 spawnPoint.x = 1;
                 spawnPoint.y = UnityEngine.Random.value;
                 direction = new Vector2(-1f, UnityEngine.Random.Range(-1, 1));
+                force = sideForce;
                 break;
             case 2:
                 //bottom
                 spawnPoint.x = UnityEngine.Random.value;
                 spawnPoint.y = 0;
                 direction = new Vector2(UnityEngine.Random.Range(-1, 1), 1f);
+                force = topForce;
                 break;
             case 3:
                 //top
                 spawnPoint.x = UnityEngine.Random.value;
                 spawnPoint.y = 1;
                 direction = new Vector2(UnityEngine.Random.Range(-1, 1), -1f);
+                force = topForce;
                 break;
 
                 //in viewport space (0,1) and need to have it in world space.
@@ -72,7 +79,7 @@ public class AsteroidSpawner : MonoBehaviour
             worldSpawnPoint, 
             Quaternion.Euler(0f,0f,UnityEngine.Random.Range(0,360)));
         Rigidbody rb = asteroidInstance.GetComponent<Rigidbody>();
-        rb.velocity = direction.normalized * UnityEngine.Random.Range(forceRange.x, forceRange.y);
+        rb.velocity = direction.normalized * UnityEngine.Random.Range(force.x, force.y);
         //normalizing to make sure the magnitude is 1
 
     }
